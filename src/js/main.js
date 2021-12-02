057f6842415dd81c76faa72c9700558a2d452f98
timur <timurkakhkharov@gmail.com>
2021-12-02 19:35:26 +0500

    vpn6

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,30 @@ header.addEventListener('click', () => {
   links.classList.remove('active')
 })
 
+// SWIPER
+
+// $('.center').slick({
+//   centerMode: true,
+//   centerPadding: '60px',
+//   slidesToShow: 3,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 3
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         arrows: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1
+//       }
+//     }
+//   ]
+// // });
